165c329b0e78d9075dcb0317a4da97f2749e082a
Vincent-Antoine <vincentcomparato69@gmail.com>
2023-10-02 13:48:18 +0200

    animation en JS V3

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,6 @@ let titre = document.getElementById("title");
 function change() {
   titre.style.color = "yellow";
   setTimeout(() => {
-    titre.style.color = "yellow";
-  }, 2000);
-  setTimeout(() => {
     titre.style.color = "blue";
   }, 2000);
 }
